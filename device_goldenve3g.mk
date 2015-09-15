57f467c444a3e9c87cf1f8f21108cf093c36fd22
MasterPlexus <MasterPlexus@users.noreply.github.com>
2015-09-15 12:02:44 +0200

    Update and rename device_lt023g.mk to device_goldenve3g.mk
    Change to goldenve

--- device_goldenve3g.mk
+++ device_goldenve3g.mk
@@ -3,9 +3,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/samsung/lt02wifi/lt02wifi-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/goldenvewifi/goldenvewifi-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/samsung/lt02wifi/overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/goldenvewifi/overlay
 
 # LOCAL_PATH := device/samsung/lt02wifi
 # ifeq ($(TARGET_PREBUILT_KERNEL),)
@@ -21,8 +21,8 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/lt02wifi/overlay
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_lt02wifi
-PRODUCT_DEVICE := lt02wifi
+PRODUCT_NAME := full_goldenvewifi
+PRODUCT_DEVICE := goldenvewifi
 
 # Charger
 PRODUCT_PACKAGES += \
